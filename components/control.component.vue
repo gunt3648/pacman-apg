5315091a6d037f39f9e96fa7e5b0505f99bdff89
Gunt Chanmas <gunt4826@outlook.com>
2021-12-23 20:49:39 +0700

    Add help button

--- components/control.component.vue
+++ components/control.component.vue
@@ -3,6 +3,54 @@
 		<v-col cols="12" class="py-0 pb-1">
 			<p class="ma-0">
 				Quick commands
+				<v-dialog v-model="dialog" width="600">
+					<template #activator="{ on, attrs }">
+						<v-btn
+							class="ma-1 cmd-small-btn"
+							width="16"
+							height="16"
+							outlined
+							fab
+							color="white"
+							v-bind="attrs"
+							v-on="on"
+						>
+							<v-icon size="14">
+								mdi-help
+							</v-icon>
+						</v-btn>
+					</template>
+					<v-card>
+						<v-card-title class="text-h5">
+							Available commands
+						</v-card-title>
+						<v-text-field
+							v-model="search"
+							append-icon="mdi-magnify"
+							label="Search for commands"
+							single-line
+							hide-details
+							class="mx-6 my-2"
+						/>
+
+						<v-data-table
+							dense
+							:headers="headers"
+							:footer-props="footers"
+							:items="commands"
+							:search="search"
+							item-key="name"
+							height="360"
+							class="mx-6"
+						/>
+						<v-card-actions>
+							<v-spacer />
+							<v-btn color="white" text @click="dialog = false">
+								Close
+							</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-dialog>
 			</p>
 		</v-col>
 		<v-col
@@ -39,6 +87,21 @@ import { getTimeInDifferentTZ } from '~/utils/pacman.function'
 export default class ControlComponent extends Vue {
 	@Prop({ required: true }) commands!: any
 	@Prop() userData!: HelixPrivilegedUser
+	public dialog: boolean = false
+	public headers: any[] = [
+		{ text: 'ID', align: 'start', filterable: false, value: 'id' },
+		{ text: 'Commands', value: 'name' },
+		{
+			text: 'Description',
+			sortable: false,
+			filterable: false,
+			value: 'description'
+		}
+	]
+	public footers: any = {
+		'disable-items-per-page': true
+	}
+	public search: string = ''
 
 	async sendCommand (message: any) {
 		const messageRef = this.$fire.database.ref('commands')
@@ -51,7 +114,7 @@ export default class ControlComponent extends Vue {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .cmd-inpadding {
 	padding-left: 4px;
 	padding-right: 4px;
